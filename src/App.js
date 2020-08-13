164b350f9dae54621eaa87aafd07ec4fd5df8941
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2020-08-13 15:49:01 -0500

    Set up Sidebar
    Get Material UI for icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./App.css";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>Ready to Build a Twitter Clone!</h1>
+
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Feed */}
+
+      {/* Widgets */}
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Sidebar.css";
+import TwitterIcon from "@material-ui/icons/Twitter";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      {/* Twitter Icon */}
+      <TwitterIcon />
+
+      {/* SidebarOption */}
+
+      {/* Tweet Button */}
+    </div>
+  );
+}
+
+export default Sidebar;
