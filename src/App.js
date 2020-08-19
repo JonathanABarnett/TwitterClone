23fbf25c16a41d5d23c88798d6c886b906e58829
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2020-08-19 14:24:59 -0500

    Build out Feed and Sidebar components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
 import "./App.css";
 import Sidebar from "./Sidebar";
+import Feed from "./Feed";
+import Widgets from "./Widgets";
 
 function App() {
   return (
     <div className="app">
-      <h1>Ready to Build a Twitter Clone!</h1>
-
       {/* Sidebar */}
       <Sidebar />
 
       {/* Feed */}
+      <Feed />
 
       {/* Widgets */}
+      <Widgets />
     </div>
   );
 }
---
+++ src/Feed.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Feed.css";
+
+function Feed() {
+  return (
+    <div className="feed">
+      {/* Header */}
+      <div className="feed__header">
+        <h2>Home</h2>
+      </div>
+
+      {/* Tweetbox */}
+
+      {/* Post */}
+    </div>
+  );
+}
+
+export default Feed;
